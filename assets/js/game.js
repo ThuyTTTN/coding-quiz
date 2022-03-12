31cf188f3f8fea80cba361985612f417e0f1e55a
Thuy Nguyen <ttruong06@gmail.com>
2022-03-12 13:51:00 -0500

    html and css for game, endgame, and highscore

---
+++ assets/js/game.js
@@ -0,0 +1,55 @@
+var question = document.querySelector("game-q");
+var choices = array.from(document.querySelectorAll("choices");
+var scoreText = document.querySelector("score");
+var timerEl = document.querySelector("timer");
+
+
+// var currentQ = {}
+// var correctA = true
+// var score = 0
+// var qCounter = 0
+
+
+
+// var questions = [
+//     {
+//         question:  "1. Which of the following is not considered a JavaScript popup box?", 
+//         choice1: "Alert box",
+//         choice2: "Flex box",
+//         choice3: "Confirm box",
+//         choice4: "Prompt box",
+//         answer: 2,
+//     },
+//     {
+//         question:  "Question 2", 
+//         choice1: "Alert box",
+//         choice2: "Flex box",
+//         choice3: "Confirm box",
+//         choice4: "Prompt box",
+//         answer: 1,
+//     },
+//     {
+//         question:  "Question 3", 
+//         choice1: "Alert box",
+//         choice2: "Flex box",
+//         choice3: "Confirm box",
+//         choice4: "Prompt box",
+//         answer: 4,
+//     },
+//     {
+//         question:  "Question 4", 
+//         choice1: "Alert box",
+//         choice2: "Flex box",
+//         choice3: "Confirm box",
+//         choice4: "Prompt box",
+//         answer: 5,
+//     },
+//     {
+//         question:  "Question 5", 
+//         choice1: "Alert box",
+//         choice2: "Flex box",
+//         choice3: "Confirm box",
+//         choice4: "Prompt box",
+//         answer: 3,
+//     },
+// ]
